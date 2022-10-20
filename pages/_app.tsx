2602400b55dfb2cdc55f12c41e75c692ea8f5c00
Bioły <41907457+Biolasek@users.noreply.github.com>
2022-10-21 00:25:29 +0200

    Fixing css prio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/globals.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { useEffect } from 'react';
